b18853bd5343b583ee916537fb5389bab643316c
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-06-16 06:31:01 +0600

    not finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import ImageCard from './Components/ImageCard';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect (() =>{
+    fetch(`https://pixabay.com/api/?key=22093135-a106e70df79bab2533f056ffb&q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json())
+    .then(data => {
+      setImages(data.hits);
+      setIsLoading(false)
+    })
+    .catch(err => console.log(err))
+  },[]);
+
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://wallpaperaccess.com/full/1391981.jpg" alt="" className="w-full" /> 
-      <div className="mx-auto">
-        <div className="mx-auto font-bold text-purple-500 text-lg mb-2">
-          Photo of Swan
-        </div>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {
+          images.map(image => (<ImageCard/>))
+        }
       </div>
     </div>
   );
